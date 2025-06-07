4c0e25f4840626aede81518d43a1af59b77f9843
Youngchan0503 <lgihtyear@gmail.com>
2025-06-07 13:09:06 +0900

    아무것도 아님

--- src/front_code/src/services/apiClient.js
+++ src/front_code/src/services/apiClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", // 실제 서버 주소에 맞게 수정
+  baseURL: process.env.REACT_APP_API_BASE_URL, // 실제 서버 주소에 맞게 수정
   headers: {
     "Content-Type": "application/json",
   },
